683074f5dd6fc8391695cec80a689f4eaee5173f
Michael Sievers <michael_sievers@web.de>
2011-07-22 14:17:07 +0200

    Added Dojo includes in order to use Dijit widgets later on.

--- etherpad/src/plugins/headings/hooks.js
+++ etherpad/src/plugins/headings/hooks.js
@@ -1,4 +1,5 @@
 import("etherpad.globals.*");
+import("etherpad.helpers");
 import("etherpad.log");
 import("etherpad.utils.*");
 import("dispatch.{Dispatcher,PrefixMatcher,forward}");
@@ -6,4 +7,8 @@ import("faststatic");
 
 function editBarItemsLeftPad(arg) {
   return arg.template.include('headingsPluginEditbarControls.ejs', undefined, ['headings']);
+
+  // TODO: This does not seem to be the right place for this
+  helpers.addToHead('<script src="http://ajax.googleapis.com/ajax/libs/dojo/1.6/dojo/dojo.xd.js" djConfig="parseOnLoad: true"></script>');
+  helpers.addToHead('<link rel="stylesheet" type="text/css" href="http://ajax.googleapis.com/ajax/libs/dojo/1.6/dijit/themes/claro/claro.css" />');
 }
