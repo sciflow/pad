56fd4ecf8a87fa252cf33de1009171c96f4843c5
Michael Sievers <michael_sievers@web.de>
2011-07-22 14:57:43 +0200

    Fixed an error which prevented to Dojo files from beeing loaded during an early return statement.

--- etherpad/src/plugins/headings/hooks.js
+++ etherpad/src/plugins/headings/hooks.js
@@ -6,8 +6,6 @@ import("dispatch.{Dispatcher,PrefixMatcher,forward}");
 import("faststatic");
 
 function editBarItemsLeftPad(arg) {
-  return arg.template.include('headingsPluginEditbarControls.ejs', undefined, ['headings']);
-
   // TODO: This does not seem to be the right place for this
   helpers.addToHead('<script src="http://ajax.googleapis.com/ajax/libs/dojo/1.6/dojo/dojo.xd.js" djConfig="parseOnLoad: true"></script>');
   helpers.addToHead('<link rel="stylesheet" type="text/css" href="http://ajax.googleapis.com/ajax/libs/dojo/1.6/dijit/themes/claro/claro.css" />');
@@ -17,4 +15,6 @@ function editBarItemsLeftPad(arg) {
   
   // TODO: maybe one could bind that class only to the controls, not to the whole body
   helpers.addBodyClass("claro");
+
+  return arg.template.include('headingsPluginEditbarControls.ejs', undefined, ['headings']);
 }
