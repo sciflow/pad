5f03e6730891498f48a9afcc95c3a790dec531b3
Michael Sievers <michael_sievers@web.de>
2011-07-22 14:12:29 +0200

    Reordered the imports alphabetically

--- etherpad/src/plugins/headings/hooks.js
+++ etherpad/src/plugins/headings/hooks.js
@@ -1,8 +1,8 @@
-import("etherpad.log");
-import("faststatic");
-import("etherpad.utils.*");
 import("etherpad.globals.*");
+import("etherpad.log");
+import("etherpad.utils.*");
 import("dispatch.{Dispatcher,PrefixMatcher,forward}");
+import("faststatic");
 
 function editBarItemsLeftPad(arg) {
   return arg.template.include('headingsPluginEditbarControls.ejs', undefined, ['headings']);
