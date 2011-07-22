f96708951f97460373b718a90bc1cd47b7b3819f
Michael Sievers <michael_sievers@web.de>
2011-07-22 14:31:50 +0200

    Fixed some CSS issues using the Dijit widgets in conjunction with Etherpad (taken from github.com/xmbms/pad)

--- etherpad/src/plugins/headings/hooks.js
+++ etherpad/src/plugins/headings/hooks.js
@@ -11,4 +11,10 @@ function editBarItemsLeftPad(arg) {
   // TODO: This does not seem to be the right place for this
   helpers.addToHead('<script src="http://ajax.googleapis.com/ajax/libs/dojo/1.6/dojo/dojo.xd.js" djConfig="parseOnLoad: true"></script>');
   helpers.addToHead('<link rel="stylesheet" type="text/css" href="http://ajax.googleapis.com/ajax/libs/dojo/1.6/dijit/themes/claro/claro.css" />');
+
+  // This fixes some Dijit CSS issues - taken from github.com/xmbms/pad
+  helpers.addToHead('<link rel="stylesheet" type="text/css" href="/static/css/plugins/headings/dijitClaroThemePatch.css" />');
+  
+  // TODO: maybe one could bind that class only to the controls, not to the whole body
+  helpers.addBodyClass("claro");
 }
